269ff48e9bad6516506168f9073cc491cb5e703f
SharaGB <shara.garcia.betancur@gmail.com>
2021-12-07 11:18:26 -0500

    Test Shara

--- Tests/test_sharagarcia.sh
+++ Tests/test_sharagarcia.sh
@@ -7,25 +7,25 @@ simple_shell=$(echo "pwd" | ./hsh);
 sh=$(echo "pwd" | sh);
 if [ "${simple_shell}" == "${sh}" ]
 then
-    echo Check 0 :white_check_mark:
+    echo Check 0 ✔️
 else
-    echo Check 0 :x:
+    echo Check 0 ❌
 fi
 
 simple_shell=$(echo "ls" | ./hsh);
 sh=$(echo "ls" | sh);
 if [ "${simple_shell}" == "${sh}" ]
 then
-    echo Check 1 :white_check_mark:
+    echo Check 1 ✔️
 else
-    echo Check 1 :x:
+    echo Check 1 ❌
 fi
 
 simple_shell=$(echo "/bin/env" | ./hsh);
 sh=$(echo "/bin/env" | sh);
 if [ "${simple_shell}" == "${sh}" ]
 then
-    echo Check 2 :white_check_mark:
+    echo Check 2 ✔️
 else
-    echo Check 2 :x:
+    echo Check 2 ❌
 fi
