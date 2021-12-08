f02653a7ab48a4781f6368e18364399e5b3e3df7
anfepema700 <anfepema700>
2021-12-08 16:44:54 -0500

    test_andresfelipeperez.sh

---
+++ Tests/test_andresfelipeperez.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+echo "cd -" | sh
+echo "cd -" | ./hsh
+
+echo "find . -type d" | sh
+echo "find . -type d" | ./hsh
+
+echo "ls -al . .. /boot" | sh
+echo "ls -al . .. /boot" | ./hsh
+
+echo "mkdir -p" | sh
+echo "mkdir -p" | ./hsh
+
