1606d59255f982345d17ff068d2efaeb1e89d369
CarlosRinconsofdev <3901@holbertonschool.com>
2021-12-08 20:22:00 -0500

    passwd, cp -r, du -ks, man ls

--- Tests/test_carlosrincon.sh
+++ Tests/test_carlosrincon.sh
@@ -3,11 +3,11 @@
 echo "passwd" | sh
 echo "passwd" | ./hsh
 
-echo "cp -r my_dir my_dir_2" | ./hsh
+echo "cp -r my_dir my_dir_2" | sh
 echo "cp -r my_dir my_dir_2" | ./hsh
 
-echo "du -ks my_dir/my_dir2" | ./hsh
+echo "du -ks my_dir/my_dir2" | sh
 echo "du -ks my_dir/my_dir2" | ./hsh
 
-echo "man ls" | ./hsh
+echo "man ls" | sh
 echo "man ls" | ./hsh
