88b17feff4011d750947456206f1c3b8ac0a5f43
fyga10 <frankgiraldo62@outlook.com>
2021-12-08 21:23:09 -0500

    repeated word fixed

--- Tests/test_frankgiraldo.sh
+++ Tests/test_frankgiraldo.sh
@@ -3,12 +3,12 @@
 echo "eject cdrom" | sh
 echo "eject cdrom" | ./hsh
 
-echo "cd .." | ./hsh
+echo "cd .." | sh
 echo "cd .." | ./hsh
 
-echo "apropos zip" | ./hsh
+echo "apropos zip" | sh
 echo "apropos zip" | ./hsh
 
-echo "df -h" | ./hsh
+echo "df -h" | sh
 echo "df -h" | ./hsh
 
